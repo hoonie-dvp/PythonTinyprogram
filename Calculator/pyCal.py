2f94625bcb733138af96e783911ff3026e4a4040
hoonie-dvp <sihun457@gmail.com>
2025-09-30 14:01:36 +0100

    First Application

--- Calculator/pyCal.py
+++ Calculator/pyCal.py
@@ -1,4 +1,17 @@
-print("==================================")
-print("Welcome to the Python Calculator")
-print("==================================")
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
+def demo():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(300, 300, 300, 300)
+    win.setWindowTitle("Python demo")
+    label = QtWidgets.QLabel(win)
+    label.setText("Hello World")
+    label.move(100, 100)
+
+    win.show()
+    sys.exit(app.exec_())
+
+demo()
